35fb51e671c43efaa2a35d96d3ead62fa451f3d7
SHIBINSHA <shibin24888@gmail.com>
2023-10-28 21:57:39 +0530

    update

---
+++ Readme.md
@@ -0,0 +1,27 @@
+# Hangman Game
+
+![Hangman](images/entry.png)
+
+Hangman is a classic word-guessing game where one player thinks of a word and the other player tries to guess it letter by letter. The game continues until the guesser either guesses the word correctly or makes too many incorrect guesses.
+
+## Features
+
+- Single-player mode
+- Multiplayer mode (coming soon)
+- A wide range of words to guess
+- Keep track of your score
+- User-friendly interface
+
+## How to Play
+
+1. Start the game by running the program.
+2. Select the game mode (single-player or multiplayer).
+3. In single-player mode, the computer will choose a random word for you to guess.
+4. Guess letters one by one. You can keep track of your incorrect guesses.
+5. Keep guessing until you either guess the word correctly or run out of attempts.
+6. In multiplayer mode, one player thinks of a word and the other player guesses it.
+7. Take turns and have fun!
+
+
+![Hangman on](images/repeat.png)
+![Hangman on](images/exit.png)
--- code/main.py
+++ code/main.py
@@ -2,19 +2,19 @@
 import random
 from hangman_words import word_list
 chosen_word = random.choice(word_list)
-print(f"the solution of the choosen word is{chosen_word}")
 #generated a random word from hangman_words.py
 from hangman_art import logo
 print (logo)
 word_len = len(chosen_word)
 display=[]
-print (word_len)
+
 for _ in range(word_len):
     display += "_"
 end_game=False
-lives=6    
+lives=6   
+print("".join(display),"\n") 
 while not end_game:
-    guess=input("guess a letter").lower() 
+    guess=input("Guess a letter:").lower() 
     if guess in display:
         print("you have already entered")
     for position in range(word_len):
@@ -27,9 +27,9 @@ while not end_game:
         if lives ==0:
             end_game=True
             print("fool you got failed")
+            print(chosen_word," was the word.")
       
-    print(f"{''.join(display)}")
-    print(chosen_word)
+    print(f"{''.join(display)}\n")
     if "_" not in display:
           end_game=True
           print("you win")          
